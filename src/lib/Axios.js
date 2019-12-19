55e319436c9887541eb6b8d0e323e5e0b0ae3234
Michael Dwyer <michael.todd.dwyer@gmail.com>
2019-12-19 10:32:12 -0500

    port 4000

--- src/lib/Axios.js
+++ src/lib/Axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const Axios = axios.create({
-    baseURL: process.env.NODE_ENV === 'development' ? 'http://localhost:3001/' : '',
+    baseURL: 'http://localhost:4000/',
     timeout: 50000
 })
 
